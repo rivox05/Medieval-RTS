4155f9b19cfcd4b51a0a1e04da11c4396a8bd09c
rivox05 <ffg041539@gmail.com>
2026-07-14 12:44:35 +0000

    Update bot status message

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "Made by rivox0_0",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
